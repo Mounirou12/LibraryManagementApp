ea2f15f6a8f930dc6df91ebb1de65067901454b3
FAGBEMY MOUNIROU <fagbemymounirou@gmail.com>
2026-09-22 19:56:01 +0100

    Modification de la requête SQL pour récupérer tous les livres et ajout de la modification et de la suppression d'un livre par ID (CRUD)

--- src/main/java/com/projet/Main.java
+++ src/main/java/com/projet/Main.java
@@ -26,15 +26,33 @@ public class Main {
             System.out.println("Connection established successfully");// message de confirmation de connexion vers la
                                                                       // base de données
 
-            String query = "SELECT title FROM Books WHERE id=1";// La requête SQL vers la base de données
+            String query = "SELECT * FROM Books";// La requête SQL vers la base de données
+            // String queryIn = "INSERT INTO Books (title, author, category, status) "
+            // + "VALUES ('Les Misérables', 'Victor Hugo', 'Classique', 'EMPRUNTE')";// La requête SQL vers la base de donnees pour ajouter un livre
+            // String queryUp = "UPDATE Books SET status='DISPONIBLE' WHERE id=5";// La requête SQL vers la base de donnees pour mettre a jour un livre
+            String sqlDl = "DELETE FROM Books WHERE ID = 6";// La requête SQL vers la base de données pour supprimer un livre
             stmt = conn.createStatement();// Création du statement SQL vers la base de données
+            // boolean status = stmt.execute(queryIn);// Exécuter la requête SQL vers la base de données
+            // System.out.println("Execute status: " + status); // Afficher le statut de l'execution de la requête SQL vers la base de données
+            // int rowsUpdate = stmt.executeUpdate(queryUp);// Exécuter la requête SQL vers la base de données
+            int rowsDeleted = stmt.executeUpdate(sqlDl);// Exécuter la requête SQL vers la base de données
             rs = stmt.executeQuery(query);// Exécuter la requête SQL vers la base de données
+            // System.out.println("Rows Updated: "+rowsUpdate);
+            System.out.println("Rows deleted: " + rowsDeleted);// Afficher le nombre de lignes affectées par la requête SQL vers la base de données
 
-            if (rs.next()) {// Si le resultat de la requête SQL est trouvé
-                String title = rs.getString("title");// Recupérer le titre du livre
-                System.out.println("Titre du livre: " + title);// Afficher le titre du livre
-            } else {
-                System.out.println("No book found with id = 1");// message de confirmation de chargement du pilote
+            // if (rs.next()) {// Si le resultat de la requête SQL est trouvé
+            // String title = rs.getString("title");// Recupérer le titre du livre
+            // System.out.println("Titre du livre: " + title);// Afficher le titre du livre
+            // } else {
+            // System.out.println("No book found with id = 1");// message de confirmation de
+            // chargement du pilote
+            // }
+            while (rs.next()) {
+                System.out.print(rs.getInt(1) + " - ");
+                System.out.print(rs.getString(2) + " - ");
+                System.out.print(rs.getString(3) + " - ");
+                System.out.print(rs.getString(4) + " - ");
+                System.out.println(rs.getString(5));
             }
         } catch (Exception e) {// Gestion des exceptions de chargement du pilote
             System.out.println("Error: " + e.getMessage());// message d'erreur de chargement du pilote
