afe2cbb350ef0b838c3d5d269aa22ce2eba66661
FAGBEMY MOUNIROU <fagbemymounirou@gmail.com>
2026-09-23 14:34:29 +0100

    Ajout des méthodes CRUD pour la gestion des membres, incluant l'insertion, la mise à jour, la suppression et l'affichage des membres.

--- src/main/java/com/projet/Members.java
+++ src/main/java/com/projet/Members.java
@@ -8,14 +8,125 @@ import java.sql.SQLException;
 import java.time.LocalDate;
 
 public class Members {
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/Librairie";
+    private static final String USERNAME = "mounir";
+    private static final String PASSWORD = "agnila10";
+
+    public static int insertMember(String firstName, String lastName, String email, String phone,
+            LocalDate membershipDate, String status) {
+        String queryCR = "INSERT INTO Members(firstName,lastName,email,phone,membershipDate,status)"
+                + " VALUES(?,?,?,?,?,?)";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            PreparedStatement pstmt = conn.prepareStatement(queryCR);
+            pstmt.setString(1, firstName);
+            pstmt.setString(2, lastName);
+            pstmt.setString(3, email);
+            pstmt.setString(4, phone);
+            pstmt.setObject(5, membershipDate.now());
+            pstmt.setString(6, status);
+            return pstmt.executeUpdate();
+
+        } catch (SQLException | ClassNotFoundException e) {
+            System.out.println("Erreur INSERT Member : " + e.getMessage());
+            return 0;
+        }
+    }
+
+    public static int updateMember(int id, String firstName, String lastName, String email, String phone,
+            LocalDate membershipDate, String status) {
+        String queryUp = "UPDATE Members SET firstName=?,lastName=?,email=?,phone=?,membershipDate=?, status = ? WHERE id=?";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            PreparedStatement pstmt = conn.prepareStatement(queryUp);
+            pstmt.setString(1, firstName);
+            pstmt.setString(2, lastName);
+            pstmt.setString(3, email);
+            pstmt.setString(4, phone);
+            pstmt.setObject(5, membershipDate.now());
+            pstmt.setString(6, status);
+            pstmt.setInt(7, id);
+            return pstmt.executeUpdate();
+
+        } catch (SQLException | ClassNotFoundException e) {
+            System.out.println("Erreur UPDATE Member : " + e.getMessage());
+            return 0;
+        }
+    }
+
+    public static int deleteMember(int id) {
+        String queryDl = "DELETE FROM Members WHERE id = ?";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            PreparedStatement pstmt = conn.prepareStatement(queryDl);
+            pstmt.setInt(1, id);
+            return pstmt.executeUpdate();
+
+        } catch (SQLException | ClassNotFoundException e) {
+            System.out.println("Erreur DELETE Member : " + e.getMessage());
+            return 0;
+        }
+    }
+
+    public static void getMemberById(int id) {
+        String sqlRdId = "SELECT id,firstName,lastName,email,phone,membershipDate,status FROM Books WHERE id = ?";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            PreparedStatement pstmt = conn.prepareStatement(sqlRdId);
+            pstmt.setInt(1, id);
+            try (ResultSet rs = pstmt.executeQuery()) {
+                if (rs.next()) {
+                    System.out.println(rs.getInt(1) + " - "
+                            + rs.getString(2) + " - "
+                            + rs.getString(3) + " - "
+                            + rs.getString(4) + " - "
+                            + rs.getString(5) + " - "
+                            + rs.getDate(6) + " - "
+                            + rs.getDate(7));
+                } else {
+                    System.out.println("Aucun livre avec l'id " + id);
+                }
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            System.out.println("Erreur SELECT MEMBER : " + e.getMessage());
+        }
+    }
+
+    public static void getAllMembers() {
+        String queryRD = "SELECT * From Members";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            PreparedStatement pstmt = conn.prepareStatement(queryRD);
+            ResultSet rs = pstmt.executeQuery(queryRD);
+            while (rs.next()) {
+                System.out.print(rs.getInt(1) + " - ");
+                System.out.print(rs.getString(2) + " - ");
+                System.out.print(rs.getString(3) + " - ");
+                System.out.print(rs.getString(4) + " - ");
+                System.out.print(rs.getString(5) + " - ");
+                System.out.print(rs.getDate(6) + " - ");
+                System.out.println(rs.getString(7));
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+    }
+
     public static void main(String[] args) {
-        crudMembers();
+        insertMember("Camara", "Sefou", "sefoucamara@email.com", "+22997676534", LocalDate.now(), "ACTIF");
+        updateMember(4, "Traore", "Amadou", "amadoutraore@email.com", "+22997676534", LocalDate.now(), "ACTIF");
+        deleteMember(2);
+        getMemberById(3);
+        getAllMembers();
     }
 
     public static void crudMembers() {
-        String url = "jdbc:mysql://127.0.0.1:3306/Librairie";
-        String username = "mounir";
-        String password = "agnila10";
 
         Connection conn = null;
         PreparedStatement pstmt = null;
@@ -23,7 +134,7 @@ public class Members {
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            conn = DriverManager.getConnection(url, username, password);
+            conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
             String queryRD = "SELECT * From Members";
             /*
              * String queryCR =
