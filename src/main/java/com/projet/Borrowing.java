4928a9552780e99a62bd6eee4c3a75377e61ee0f
FAGBEMY MOUNIROU <fagbemymounirou@gmail.com>
2026-09-23 20:37:40 +0100

    Ajout des paramètres pour les dates dans les méthodes insertMember et updateMember, et mise à jour des appels dans main.

--- src/main/java/com/projet/Borrowing.java
+++ src/main/java/com/projet/Borrowing.java
@@ -6,76 +6,128 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.Locale;
 
 public class Borrowing {
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/Librairie";
+    private static final String USERNAME = "mounir";
+    private static final String PASSWORD = "agnila10";
 
-    public static void main(String[] args) {
-        crudBorrowing();
-    }
-
-    public static void crudBorrowing() {
-        String url = "jdbc:mysql://127.0.0.1:3306/Librairie";
-        String username = "mounir";
-        String password = "agnila10";
-
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-
+    public static int insertBorrowing(int bookId, int memberId, LocalDate borrowDate, LocalDate dueDate,
+            Locale returnDate, String status, int year, int month, int day) {
+        borrowDate = LocalDate.of(year, month, day);
+        dueDate = borrowDate.plusDays(14);
+        String queryCR = "INSERT INTO Borrowing(bookId,memberId,borrowDate,dueDate,returnDate,status)"
+                + " VALUES(?,?,?,?,?,?)";
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            conn = DriverManager.getConnection(url, username, password);
-            String queryRD = "SELECT * From Borrowing";
-
-            String queryCR = "INSERT INTO Borrowing(bookId,memberId,borrowDate,dueDate,returnDate,status)"
-                    + " VALUES(?,?,?,?,?,?)";
-            pstmt = conn.prepareStatement(queryCR);
-            pstmt.setInt(1, 5);
-            pstmt.setInt(2, 3);
-            pstmt.setObject(3, LocalDate.now());
-            pstmt.setObject(4, LocalDate.now().plusDays(14));
-            pstmt.setObject(5, null);
-            pstmt.setString(6, "EN_COURS");
-
-            int rowsAffected = pstmt.executeUpdate();
-            System.out.println("Rows insetrted" + rowsAffected);
-
-            String queryUp = "UPDATE Borrowing SET status = ? WHERE id=?";
-            pstmt = conn.prepareStatement(queryUp);
-            pstmt.setString(1, "RETOURNE");
-            pstmt.setInt(2, 1);
-            pstmt.executeUpdate();
-
-            String queryDl = "DELETE FROM Borrowing WHERE id = ?";
-            pstmt = conn.prepareStatement(queryDl);
-            pstmt.setInt(1, 2);
-
-            pstmt.executeUpdate();
-            rs = pstmt.executeQuery(queryRD);
-            while (rs.next()) {
-                System.out.print(rs.getInt(1) + " - ");
-                System.out.print(rs.getString(2) + " - ");
-                System.out.print(rs.getString(3) + " - ");
-                System.out.print(rs.getString(4) + " - ");
-                System.out.print(rs.getString(5) + " - ");
-                System.out.print(rs.getDate(6) + " - ");
-                System.out.println(rs.getString(7));
-            }
-        } catch (Exception e) {
-            System.out.println("Error: " + e.getMessage());
-            e.printStackTrace();
-        } finally {
-            try {
-                if (rs != null)
-                    rs.close();
-                if (pstmt != null)
-                    pstmt.close();
-                if (conn != null && !conn.isClosed())
-                    conn.close();
-            } catch (SQLException e) {
-                System.out.println("Error closing connection: " + e.getMessage());
-            }
+            Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            PreparedStatement pstmt = conn.prepareStatement(queryCR);
+            pstmt.setInt(1, bookId);
+            pstmt.setInt(2, memberId);
+            pstmt.setObject(3, borrowDate);
+            pstmt.setObject(4, dueDate);
+            pstmt.setObject(5, returnDate);
+            pstmt.setString(6, status);
+            return pstmt.executeUpdate();
+        } catch (SQLException | ClassNotFoundException e) {
+            System.out.println("Erreur INSERT Borrowing : " + e.getMessage());
+            return 0;
         }
     }
 
+    public static int updateBorrowing(int id, int bookId, int memberId, LocalDate borrowDate, LocalDate dueDate,
+            Locale returnDate, String status, int year, int month, int day) {
+        borrowDate = LocalDate.of(year, month, day);
+        dueDate = borrowDate.plusDays(14);
+        String queryUp = "UPDATE Borrowing SET bookId=?,memberId=?,borrowDate=?,dueDate=?, returnDate= ?, status = ? WHERE id=?";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            PreparedStatement pstmt = conn.prepareStatement(queryUp);
+            pstmt.setInt(1, bookId);
+            pstmt.setInt(2, memberId);
+            pstmt.setObject(3, borrowDate);
+            pstmt.setObject(4, dueDate);
+            pstmt.setObject(5, returnDate);
+            pstmt.setString(6, status);
+            pstmt.setInt(7, id);
+            return pstmt.executeUpdate();
+        } catch (SQLException | ClassNotFoundException e) {
+            System.out.println("Erreur UPDATE Borrowing : " + e.getMessage());
+            return 0;
+        }
+    }
+
+    public static int deleteBorrowing(int id) {
+        String queryDl = "DELETE FROM Borrowing WHERE id = ?";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            PreparedStatement pstmt = conn.prepareStatement(queryDl);
+            pstmt.setInt(1, id);
+            return pstmt.executeUpdate();
+
+        } catch (SQLException | ClassNotFoundException e) {
+            System.out.println("Erreur DELETE Borrowing : " + e.getMessage());
+            return 0;
+        }
+    }
+
+    public static void getBorrowingById(int id) {
+        String sqlRdId = "SELECT id,bookId,memberId,borrowDate,dueDate,returnDate,status FROM Borrowing WHERE id = ?";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            PreparedStatement pstmt = conn.prepareStatement(sqlRdId);
+            pstmt.setInt(1, id);
+            try (ResultSet rs = pstmt.executeQuery()) {
+                if (rs.next()) {
+                    System.out.println(rs.getInt(1) + " - "
+                            + rs.getInt(2) + " - "
+                            + rs.getInt(3) + " - "
+                            + rs.getDate(4) + " - "
+                            + rs.getDate(5) + " - "
+                            + rs.getDate(6) + " - "
+                            + rs.getString(7));
+                } else {
+                    System.out.println("Aucun enprunt avec l'id " + id);
+                }
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            System.out.println("Erreur SELECT Borrowing : " + e.getMessage());
+        }
+    }
+
+    public static void getAllMembers() {
+        String queryRD = "SELECT * From Borrowing";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            PreparedStatement pstmt = conn.prepareStatement(queryRD);
+            ResultSet rs = pstmt.executeQuery(queryRD);
+            while (rs.next()) {
+                System.out.print(rs.getInt(1) + " - ");
+                System.out.print(rs.getInt(2) + " - ");
+                System.out.print(rs.getInt(3) + " - ");
+                System.out.print(rs.getDate(4) + " - ");
+                System.out.print(rs.getDate(5) + " - ");
+                System.out.print(rs.getDate(6) + " - ");
+                System.out.println(rs.getString(7));
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            System.out.println("Erreur SELECT ALL Borrowing : " + e.getMessage());
+        }
+
+    }
+
+    public static void main(String[] args) {
+        insertBorrowing(5, 8, LocalDate.now(), LocalDate.now().plusDays(14), null, "EN_COURS", 2023, 1, 1);
+        updateBorrowing(7, 5, 8, LocalDate.now(), LocalDate.now().plusDays(14), null, "EN_COURS", 2023, 1, 1);
+       // deleteBorrowing(4);
+        //getBorrowingById(4);
+        getAllMembers();
+    }
+
+  
 }
--- src/main/java/com/projet/Members.java
+++ src/main/java/com/projet/Members.java
@@ -13,7 +13,8 @@ public class Members {
     private static final String PASSWORD = "agnila10";
 
     public static int insertMember(String firstName, String lastName, String email, String phone,
-            LocalDate membershipDate, String status) {
+            LocalDate membershipDate, String status,int year, int month, int day) {
+                membershipDate = LocalDate.of(year,month,day);
         String queryCR = "INSERT INTO Members(firstName,lastName,email,phone,membershipDate,status)"
                 + " VALUES(?,?,?,?,?,?)";
         try {
@@ -24,7 +25,7 @@ public class Members {
             pstmt.setString(2, lastName);
             pstmt.setString(3, email);
             pstmt.setString(4, phone);
-            pstmt.setObject(5, membershipDate.now());
+            pstmt.setObject(5, membershipDate);
             pstmt.setString(6, status);
             return pstmt.executeUpdate();
 
@@ -35,7 +36,8 @@ public class Members {
     }
 
     public static int updateMember(int id, String firstName, String lastName, String email, String phone,
-            LocalDate membershipDate, String status) {
+            LocalDate membershipDate, String status,int year, int month, int day) {
+                membershipDate = LocalDate.of(year,month,day);
         String queryUp = "UPDATE Members SET firstName=?,lastName=?,email=?,phone=?,membershipDate=?, status = ? WHERE id=?";
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -45,7 +47,7 @@ public class Members {
             pstmt.setString(2, lastName);
             pstmt.setString(3, email);
             pstmt.setString(4, phone);
-            pstmt.setObject(5, membershipDate.now());
+            pstmt.setObject(5, membershipDate);
             pstmt.setString(6, status);
             pstmt.setInt(7, id);
             return pstmt.executeUpdate();
@@ -112,79 +114,17 @@ public class Members {
                 System.out.print(rs.getDate(6) + " - ");
                 System.out.println(rs.getString(7));
             }
-        } catch (Exception e) {
-            // TODO: handle exception
+        } catch (SQLException | ClassNotFoundException e) {
+            System.out.println("Erreur SELECT ALL MEMBERS : " + e.getMessage());
         }
 
     }
 
     public static void main(String[] args) {
-        insertMember("Vincent", "Kompany", "vincentkompany@email.com", "+22990875634", LocalDate.now(), "SUSPENDU");
-        updateMember(4, "Diallo", "Amadou", "amadoutraore@email.com", "+22997676534", LocalDate.now(), "ACTIF");
+        insertMember("Vincent", "Kompany", "vincentkompany@email.com", "+22990875634", LocalDate.now(), "SUSPENDU", 2023, 1, 1);
+        updateMember(4, "Diallo", "Amadou", "amadoutraore@email.com", "+22997676534", LocalDate.now(), "ACTIF", 2023, 1, 1);
         deleteMember(6);
         getMemberById(8);
         getAllMembers();
-    }
-
-    public static void crudMembers() {
-
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-            String queryRD = "SELECT * From Members";
-            /*
-             * String queryCR =
-             * "INSERT INTO Members(firstName,lastName,email,phone,membershipDate,status)"
-             * + " VALUES(?,?,?,?,?,?)";
-             * pstmt.setString(1, "Traore");
-             * pstmt.setString(2, "Amadou");
-             * pstmt.setString(3, "amadoutraore@email.com");
-             * pstmt.setString(4, "+22997676534");
-             * pstmt.setObject(5, LocalDate.now());
-             * pstmt.setString(6, "SUSPENDU");
-             * 
-             * int rowsAffected = pstmt.executeUpdate();
-             * System.out.println("Rows insetrted" + rowsAffected);
-             */
-            String queryUp = "UPDATE Members SET status = ? WHERE id=?";
-            pstmt = conn.prepareStatement(queryUp);
-            pstmt.setString(1, "ACTIF");
-            pstmt.setInt(2, 4);
-            pstmt.executeUpdate();
-
-            String queryDl = "DELETE FROM Members WHERE id = ?";
-            pstmt = conn.prepareStatement(queryDl);
-            pstmt.setInt(1, 5);
-
-            pstmt.executeUpdate();
-            rs = pstmt.executeQuery(queryRD);
-            while (rs.next()) {
-                System.out.print(rs.getInt(1) + " - ");
-                System.out.print(rs.getString(2) + " - ");
-                System.out.print(rs.getString(3) + " - ");
-                System.out.print(rs.getString(4) + " - ");
-                System.out.print(rs.getString(5) + " - ");
-                System.out.print(rs.getDate(6) + " - ");
-                System.out.println(rs.getString(7));
-            }
-        } catch (Exception e) {
-            System.out.println("Error: " + e.getMessage());
-            e.printStackTrace();
-        } finally {
-            try {
-                if (rs != null)
-                    rs.close();
-                if (pstmt != null)
-                    pstmt.close();
-                if (conn != null && !conn.isClosed())
-                    conn.close();
-            } catch (SQLException e) {
-                System.out.println("Error closing connection: " + e.getMessage());
-            }
-        }
     }
 }
