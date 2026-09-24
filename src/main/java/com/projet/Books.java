e107e4e1b61d73b5c164049f1dd0ad3b4f070b91
FAGBEMY MOUNIROU <fagbemymounirou@gmail.com>
2026-09-24 06:37:12 +0100

    Ajout des méthodes CRUD pour la gestion des livres, incluant l'insertion, la mise à jour, la suppression et l'affichage des livres.

--- src/main/java/com/projet/Books.java
+++ src/main/java/com/projet/Books.java
@@ -5,61 +5,54 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.time.LocalDate;
 
-public class Members {
+public class Books {
     private static final String URL = "jdbc:mysql://127.0.0.1:3306/Librairie";
     private static final String USERNAME = "mounir";
     private static final String PASSWORD = "agnila10";
 
-    public static int insertMember(String firstName, String lastName, String email, String phone,
-            LocalDate membershipDate, String status,int year, int month, int day) {
-                membershipDate = LocalDate.of(year,month,day);
-        String queryCR = "INSERT INTO Members(firstName,lastName,email,phone,membershipDate,status)"
-                + " VALUES(?,?,?,?,?,?)";
+    public static int insertBook(String title, String author, String category, String status) {
+        String queryCR = "INSERT INTO Books (title, author, category, status) "
+                + "VALUES (?,?,?,?)";
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
             PreparedStatement pstmt = conn.prepareStatement(queryCR);
-            pstmt.setString(1, firstName);
-            pstmt.setString(2, lastName);
-            pstmt.setString(3, email);
-            pstmt.setString(4, phone);
-            pstmt.setObject(5, membershipDate);
-            pstmt.setString(6, status);
+            pstmt.setString(1, title);
+            pstmt.setString(2, author);
+            pstmt.setString(3, category);
+            pstmt.setString(4, status);
             return pstmt.executeUpdate();
 
         } catch (SQLException | ClassNotFoundException e) {
-            System.out.println("Erreur INSERT Member : " + e.getMessage());
+            System.out.println("Erreur INSERT Book : " + e.getMessage());
             return 0;
         }
     }
 
-    public static int updateMember(int id, String firstName, String lastName, String email, String phone,
-            LocalDate membershipDate, String status,int year, int month, int day) {
-                membershipDate = LocalDate.of(year,month,day);
-        String queryUp = "UPDATE Members SET firstName=?,lastName=?,email=?,phone=?,membershipDate=?, status = ? WHERE id=?";
+    public static int updateBook(int id, String title, String author, String category, String status) {
+        String queryUp = "UPDATE Books SET title=?,author=?,category=?,status=? WHERE id=?";// La requête SQL vers la
+                                                                                            // base de donnees pour
+                                                                                            // mettre a jour un livre
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
             PreparedStatement pstmt = conn.prepareStatement(queryUp);
-            pstmt.setString(1, firstName);
-            pstmt.setString(2, lastName);
-            pstmt.setString(3, email);
-            pstmt.setString(4, phone);
-            pstmt.setObject(5, membershipDate);
-            pstmt.setString(6, status);
-            pstmt.setInt(7, id);
+            pstmt.setString(1, title);
+            pstmt.setString(2, author);
+            pstmt.setString(3, category);
+            pstmt.setString(4, status);
+            pstmt.setInt(5, id);
             return pstmt.executeUpdate();
 
         } catch (SQLException | ClassNotFoundException e) {
-            System.out.println("Erreur UPDATE Member : " + e.getMessage());
+            System.out.println("Erreur UPDATE Book : " + e.getMessage());
             return 0;
         }
     }
 
-    public static int deleteMember(int id) {
-        String queryDl = "DELETE FROM Members WHERE id = ?";
+    public static int deleteBook(int id) {
+        String queryDl = "DELETE FROM Books WHERE id = ?";
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
@@ -68,13 +61,13 @@ public class Members {
             return pstmt.executeUpdate();
 
         } catch (SQLException | ClassNotFoundException e) {
-            System.out.println("Erreur DELETE Member : " + e.getMessage());
+            System.out.println("Erreur DELETE Book : " + e.getMessage());
             return 0;
         }
     }
 
     public static void getMemberById(int id) {
-        String sqlRdId = "SELECT id,firstName,lastName,email,phone,membershipDate,status FROM Members WHERE id = ?";
+        String sqlRdId = "SELECT id,title, author, category, status FROM Books WHERE id = ?";
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
@@ -86,20 +79,18 @@ public class Members {
                             + rs.getString(2) + " - "
                             + rs.getString(3) + " - "
                             + rs.getString(4) + " - "
-                            + rs.getString(5) + " - "
-                            + rs.getDate(6) + " - "
-                            + rs.getString(7));
+                            + rs.getString(5));
                 } else {
-                    System.out.println("Aucun membre avec l'id " + id);
+                    System.out.println("Aucun livre avec l'id " + id);
                 }
             }
         } catch (SQLException | ClassNotFoundException e) {
-            System.out.println("Erreur SELECT MEMBER : " + e.getMessage());
+            System.out.println("Erreur SELECT Books : " + e.getMessage());
         }
     }
 
     public static void getAllMembers() {
-        String queryRD = "SELECT * From Members";
+        String queryRD = "SELECT * From Books";
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
@@ -110,21 +101,20 @@ public class Members {
                 System.out.print(rs.getString(2) + " - ");
                 System.out.print(rs.getString(3) + " - ");
                 System.out.print(rs.getString(4) + " - ");
-                System.out.print(rs.getString(5) + " - ");
-                System.out.print(rs.getDate(6) + " - ");
-                System.out.println(rs.getString(7));
+                System.out.println(rs.getString(5) + " - ");
             }
         } catch (SQLException | ClassNotFoundException e) {
-            System.out.println("Erreur SELECT ALL MEMBERS : " + e.getMessage());
+            System.out.println("Erreur SELECT ALL Books : " + e.getMessage());
         }
 
     }
 
     public static void main(String[] args) {
-        insertMember("Vincent", "Kompany", "vincentkompany@email.com", "+22990875634", LocalDate.now(), "SUSPENDU", 2023, 1, 1);
-        updateMember(4, "Diallo", "Amadou", "amadoutraore@email.com", "+22997676534", LocalDate.now(), "ACTIF", 2023, 1, 1);
-        deleteMember(6);
-        getMemberById(8);
-        getAllMembers();
+        // insertBook("L'Assassin royal", "Robin Hobb", "Fantasy", "DISPONIBLE");
+        // updateBook(5, "Les Misérables", "Victor Hugo", "Classique", "EMPRUNTE");
+        // deleteBook(5);
+        getMemberById(1);
+        // getAllMembers();
     }
+
 }
