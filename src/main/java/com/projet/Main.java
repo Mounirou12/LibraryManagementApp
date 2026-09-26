4ae4dd2873641f087469bfd316d164ae5930b9c5
FAGBEMY MOUNIROU <fagbemymounirou@gmail.com>
2026-09-26 19:11:29 +0100

    Suppression des importations inutilisées pour la gestion de la base de données dans la classe Main.

--- src/main/java/com/projet/Main.java
+++ src/main/java/com/projet/Main.java
@@ -1,10 +1,5 @@
 package com.projet;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
